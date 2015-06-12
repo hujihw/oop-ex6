78f13194e5418b2b1af2afda07a84f86ea3c52db
omrikap <omrikap@gmail.com>
2015-06-12 19:23:56 +0300

    added finder with api. created constructor and data members of SuperBlock.

--- src/oop/ex6/expressions/ExpressionsDefiner.java
+++ src/oop/ex6/expressions/ExpressionsDefiner.java
@@ -1,8 +1,18 @@
 package oop.ex6.expressions;
 
+import oop.ex6.sjava_objects.SJavaObject;
+
 /**
  * @author Omri Kaplan
  */
 public class ExpressionsDefiner {
+    /**
+     * Define an S-Java expression, and do the appropriate task.
+     * @param expression    The expression we want to define.
+     * @return The object corresponding with the expression.
+     */
+    public SJavaObject defineExpression(String expression) {
 
+        return null;
+    }
 }
