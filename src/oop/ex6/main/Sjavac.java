0fbe6060a0d152d5e61b5426d493944231e81e69
Asaf Etzion <etzion.asaf@gmail.com>
2015-06-14 22:28:52 +0300

    Sjavac code and changes to WrongArguments

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -1,10 +1,31 @@
 package oop.ex6.main;
 
 /**
- * @author Omri Kaplan
+ * the Sjavac is the class from which the user runs the Sjava verifier application.
+ * @author Omri Kaplan & Asaf Eztion
  */
 public class Sjavac {
+
+    /**
+     * the main method is called by the user and it initiates the Sjava verification process.
+     * int wrong use, it throw a WrongArgumentsNumberException if the user calls it with no arguments or more than one
+     * also it caches the WrongArgumentsNumberException and BadFileException and prints 2 to out stream and the
+     * exception cause to the error stream
+     * @param args
+     */
     public static void main(String[] args) {
-        
+
+        try {
+            if (args.length != 1) {
+                throw new WrongArgumentsNumberException("the correct use of this application is with one argument");
+            Manager.getInstance().mainProcedure(args[0]);}
+
+        }
+        catch (WrongArgumentsNumberException | BadFileException exp){
+            String IO_ERROR = "2";
+            System.out.println(IO_ERROR);
+            System.err.println(exp.getMessage());
+        }
     }
 }
+
--- src/oop/ex6/main/WrongArgumentsNumberException.java
+++ src/oop/ex6/main/WrongArgumentsNumberException.java
@@ -2,11 +2,13 @@ package oop.ex6.main;
 
 import oop.ex6.sjava_objects.SJavaException;
 
+import java.io.IOException;
+
 /**
  * More than one argument is given by the user when calling "Sjavac" from the command line.
  * @author Omri Kaplan
  */
-public class WrongArgumentsNumberException extends SJavaException {
+public class WrongArgumentsNumberException extends IOException {
     public WrongArgumentsNumberException(String message) {
         super(message);
     }
