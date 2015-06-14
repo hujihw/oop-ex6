7264e4fb9f622a888b446a4ee05874df46eee863
omrikap <omrikap@gmail.com>
2015-06-14 23:22:25 +0300

    manager almost done. decide if it's a singleton or static.

--- src/oop/ex6/main/Manager.java
+++ src/oop/ex6/main/Manager.java
@@ -1,30 +1,40 @@
 package oop.ex6.main;
 
+import oop.ex6.sjava_objects.SJavaException;
+import oop.ex6.sjava_objects.blocks.MainBlock;
+import oop.ex6.sjava_objects.blocks.MethodBlock;
+
+import java.io.File;
+
 /**
  * The manager of the hole virification process. Calls the Parser an BlockParser, and holds the MainBlock.
- * Created by Asaf Etzion and Omri Kaplan
+ * Created by Asaf Etzion & Omri Kaplan
  */
 class Manager {
-    private static Manager ourInstance = new Manager();
-    //todo add file member.
-    //todo add method hash set and parent=null.
 
-    public static Manager getInstance() {
-        return ourInstance;
-    }
+    /* Data Members */
+    //todo add file member. still needed?
+    private MainBlock mainBlock; // todo add a method hash table/map. @Asaf remove if done.
 
-    private Manager() { // todo
+    /* Constructor */
+    private Manager() {
     }
 
     /**
      * the main method that runs the whole verifying procedure
      * @param sJavaFilePath the path of the sJava file
      */
-    void mainProcedure (String sJavaFilePath){ // todo
+    void mainProcedure (String sJavaFilePath) throws BadFileException{ // todo
         // todo try on the hole procedure. catch exceptions and print 1, otherwise print 2.
-        // calls the parsFile
-        // calls the parseBlock
-
+        try {
+            mainBlock = Parser.parseFile(new File(sJavaFilePath));
+            for (MethodBlock method : mainBlock.getAllMethods()) {
+                BlockParser.parseBlock(method);
+            }
+        } catch (SJavaException e) { // todo parseFile throws SJavaException.
+            System.out.println("1");
+            System.err.println(e.getMessage());
+        }
     }
 
 
