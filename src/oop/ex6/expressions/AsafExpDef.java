bcfe6dffc6abf1368f49ac30edd77de135e0dd6f
omrikap <omrikap@gmail.com>
2015-06-19 12:22:21 +0300

    removed redundant testers

--- src/oop/ex6/expressions/AsafExpDef.java
+++
@@ -1,7 +0,0 @@
-package oop.ex6.expressions;
-
-/**
- * @author Omri Kaplan
- */
-public class AsafExpDef {
-}
--- src/oop/ex6/main/BlockParser.java
+++ src/oop/ex6/main/BlockParser.java
@@ -38,20 +38,17 @@ public class BlockParser { // tester todo change to package local
      * Parse a method block.
      * @param methodBlock    the method block to parse.
      */
-    public void parseMethod(MethodBlock methodBlock) { // tester todo make package local
+    void parseMethod(MethodBlock methodBlock) {
         this.scanner = methodBlock.getScanner();
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             while (line.trim().equals("return;")){
-                System.out.println("found 'return;'"); // tester
                 line = scanner.nextLine();
                 if (line.trim().equals("}")) {
-                    System.out.println("found '}'"); // tester
                     return;
                 }
             }
             SJavaObject expressionObject = MainParser.commentsAndEmptyLinesFilter(line);
-            System.out.println(line); // tester
             System.out.println(expressionObject == null);
             if (expressionObject != null) { // note commentsAndEmptyLinesFilter() not implemented.
                 varOrBlockHandle(methodBlock, expressionObject);
@@ -63,7 +60,7 @@ public class BlockParser { // tester todo change to package local
      * Recursively parse SJava code blocks (scopes).
      * @param block    the block to parse.
      */
-    public void parseBlock(SuperBlock block) { // tester todo package-local.
+    void parseBlock(SuperBlock block) {
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.trim().equals("}")) {
@@ -79,10 +76,8 @@ public class BlockParser { // tester todo change to package local
 
     private void varOrBlockHandle(SuperBlock theBlock, SJavaObject theObject) {
         if (theObject instanceof SuperBlock) {
-            System.out.println("Got a block!"); // tester
             parseBlock((SuperBlock) theObject); // todo consider generics, not down casting.
         } else if (theObject instanceof SuperVar) {
-            System.out.println("Got a var!"); // tester
             theBlock.addVariable(theObject.getName(), (SuperVar) theObject);
         }
     }
