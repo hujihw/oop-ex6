2e0424f3b450e617b399de4ae1e4c3833e2680c3
omrikap <omrikap@gmail.com>
2015-06-19 19:00:26 +0300

    method call regex done

--- src/oop/ex6/expressions/OmriExpDef.java
+++ src/oop/ex6/expressions/OmriExpDef.java
@@ -10,17 +10,20 @@ public class OmriExpDef {
     final String VARIABLE_VALUE = ".+";
 
     final String LOOP_AND_CONDITION = "(if|while)";
-    final String NUMBERS = "\\d+\\.*\\d*";
+    final String NUMBERS = "\\d+(\\.\\d+)?";
     final String NUMBERS_OR_NAME = "(" + NUMBERS + "|" + VARIABLE_NAME + ")";
 
     final String BOOLEAN_OPERATOR = "((" + NUMBERS_OR_NAME + "\\s*(&&|\\|\\|)\\s*)+" + NUMBERS_OR_NAME + ")";
-    final String BOOLEAN_CONDITION = "(" + NUMBERS_OR_NAME + "|" + BOOLEAN_OPERATOR + ")";
-    // todo check if seed to support both boolean parameters in one line.
-    final String IF_WHILE_DECLARATION = "\\s*" + LOOP_AND_CONDITION + "\\s*\\(\\s*" + BOOLEAN_CONDITION +
+    final String IF_OR_WHILE_PARAMETER = "(" + NUMBERS_OR_NAME + "|" + BOOLEAN_OPERATOR + ")";
+    final String IF_WHILE_DECLARATION = "\\s*" + LOOP_AND_CONDITION + "\\s*\\(\\s*" + IF_OR_WHILE_PARAMETER +
             "\\s*\\)\\s*\\{\\s*";
 
     final String VARIABLE_DECLARATION = "\\s*" + VARIABLE_TYPE + "\\s+" + VARIABLE_NAME + "\\s*=\\s*(" +
             NUMBERS + "|\\S+)\\s*";
 
-    final String CALL_METHOD = "\\s*" + METHOD_NAME + "\\s*\\(";
+    final String COMMA_METHOD_PARAMETER = "((" + NUMBERS_OR_NAME + "|'.'|\".+\")\\s*,\\s*)*";
+    final String METHOD_PARAMETER = "((" + NUMBERS_OR_NAME + "|'.'|\".+\")\\s*)";
+
+    final String CALL_METHOD = "\\s*" + METHOD_NAME + "\\s*\\(\\s*((" + COMMA_METHOD_PARAMETER + METHOD_PARAMETER + ")|)\\)\\s*;\\s*";
+    final String ASSIGN_VARIABLE = ""; // todo
 }
