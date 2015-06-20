3f9d7659761ebf2beddde9bd042e4d782e902615
Asaf Etzion <etzion.asaf@gmail.com>
2015-06-20 10:52:27 +0300

    finished var declaration

--- src/oop/ex6/expressions/AsafExpDef.java
+++ src/oop/ex6/expressions/AsafExpDef.java
@@ -3,7 +3,7 @@ package oop.ex6.expressions;
 import java.util.Scanner;
 
 /**
- * @author Omri Kaplan
+ * @author Asaf Etzion
  */
 public class AsafExpDef {
 
@@ -14,13 +14,11 @@ public class AsafExpDef {
     final String VARIABLE_TYPE = "(final\\s+)?\\s*(int|double|String|boolean|char)";
     final String PARAMETER = VARIABLE_TYPE + "\\s+" + VARIABLE_NAME;
     final String NUMBERS = "-?\\d+(\\.\\d+)?";
-    final String VARIABLE_VALUE = "(true|false|\".*\"|'.'|" + NUMBERS + ")";
+    final String VARIABLE_VALUE = "(true|false|\".*\"|'.'|" + NUMBERS + "|" + VARIABLE_NAME + ")"; //todo true and false are redundant
+    final String VARIABLE_NAME_WITH_ASSINMENT_OPTION = "(" + VARIABLE_NAME + "(\\s*=\\s*" + VARIABLE_VALUE + "\\s*)?)";
 
+    final String METHOD_DECLARATION = "\\s*void\\s+" + METHOD_NAME + "\\s*\\(\\s*(((\\s*" + PARAMETER + "\\s*,\\s*)*(\\s*" + PARAMETER + "\\s*))|)\\s*\\)\\s*\\{\\s*";
 
-    final String METHOD_DECLARAATION = "\\s*void\\s+" + METHOD_NAME + "\\s*\\(\\s*(((\\s*" + PARAMETER + "\\s*,\\s*)*(\\s*" + PARAMETER + "\\s*))|)\\s*\\)\\s*\\{\\s*";
+    final String VARIABLE_DECLARATION = "\\s*" + VARIABLE_TYPE + "\\s+" + VARIABLE_NAME_WITH_ASSINMENT_OPTION + "(\\s*,\\s*" + VARIABLE_NAME_WITH_ASSINMENT_OPTION + ")*\\s*;\\s*";
 
-    final String VARIABLE_DECLARATION = VARIABLE_TYPE + "\\s+(" + VARIABLE_NAME + "(\\s*=)?)";
-
-    //final String VARIABLE_DECLARATION2 = "\\s*" + VARIABLE_TYPE + "\\s+" + VARIABLE_NAME + "\\s*=\\s*(" +
-          //  NUMBERS + "|\\S+)\\s*";
 }
