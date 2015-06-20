551afd5ad0722c9c1b2ffd3de88407265e2aa5f5
Asaf Etzion <etzion.asaf@gmail.com>
2015-06-20 10:08:16 +0300

    codding asafexpdef

--- src/oop/ex6/expressions/AsafExpDef.java
+++ src/oop/ex6/expressions/AsafExpDef.java
@@ -1,5 +1,7 @@
 package oop.ex6.expressions;
 
+import java.util.Scanner;
+
 /**
  * @author Omri Kaplan
  */
@@ -7,13 +9,18 @@ public class AsafExpDef {
 
     final String METHOD_NAME = "[a-zA-Z]\\w*";
     final String VARIABLE_NAME = "[a-z_A-Z]\\w*";
+
+
     final String VARIABLE_TYPE = "(final\\s+)?\\s*(int|double|String|boolean|char)";
-    final String VARIABLE_VALUE = ".+";
+    final String PARAMETER = VARIABLE_TYPE + "\\s+" + VARIABLE_NAME;
+    final String NUMBERS = "-?\\d+(\\.\\d+)?";
+    final String VARIABLE_VALUE = "(true|false|\".*\"|'.'|" + NUMBERS + ")";
 
 
-    final String PARAMETER = "\\s*"+VARIABLE_TYPE+"\\s+"+VARIABLE_NAME+"\\s*";
+    final String METHOD_DECLARAATION = "\\s*void\\s+" + METHOD_NAME + "\\s*\\(\\s*(((\\s*" + PARAMETER + "\\s*,\\s*)*(\\s*" + PARAMETER + "\\s*))|)\\s*\\)\\s*\\{\\s*";
 
-    final String METHOD_DECLARAATION = "\\s*void\\s+"+METHOD_NAME+"\\s*\\(\\s*((("+PARAMETER+",\\s*)*("+PARAMETER+"))|)\\s*\\)\\s*\\{\\s*";
+    final String VARIABLE_DECLARATION = VARIABLE_TYPE + "\\s+(" + VARIABLE_NAME + "(\\s*=)?)";
 
-    final String VARIABLE_DECLARATION = VARIABLE_TYPE+"\\s+"; //todo add decelare or and init- w/ multiple vars
+    //final String VARIABLE_DECLARATION2 = "\\s*" + VARIABLE_TYPE + "\\s+" + VARIABLE_NAME + "\\s*=\\s*(" +
+          //  NUMBERS + "|\\S+)\\s*";
 }
