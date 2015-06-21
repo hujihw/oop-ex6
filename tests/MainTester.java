f5de96e4883d0e1ba20236269fc81bf04c1f573d
omrikap <omrikap@gmail.com>
2015-06-21 20:39:12 +0300

    New tester file

---
+++ tests/MainTester.java
@@ -0,0 +1,21 @@
+import oop.ex6.main.Sjavac;
+
+import java.io.File;
+
+/**
+ * @author Omri Kaplan
+ */
+public class MainTester {
+    public static void main(String[] args) {
+        File sourceDir = new File("C:\\Users\\Omri\\OneDrive\\Documents\\huji\\hw\\oop\\ex6\\tests\\tests");
+        File[] tests = sourceDir.listFiles();
+        String[] argument = new String[1];
+
+        assert tests != null;
+        for (File test : tests) {
+            argument[0] = String.valueOf(test.toPath());
+            System.out.println("-------------------------------------------------------------------------");
+            Sjavac.main(argument);
+        }
+    }
+}
